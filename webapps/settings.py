8ddf7cc28dcf077d51de575f08577d6852110936
Larry7707 <larry.l1@hotmail.com>
2017-11-09 22:47:55 +0000

    email testing

--- webapps/settings.py
+++ webapps/settings.py
@@ -140,12 +140,13 @@ MEDIA_URL = '/grumblr/images/profile/'
 
 # Cofigures Django to merely print emails
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp-mail.outlook.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'larry.l1@hotmail.com'
-EMAIL_HOST_PASSWORD = 'Lxl(31207'
+#EMAIL_HOST = 'smtp.live.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'larry.12.77.07@gmail.com'
+EMAIL_HOST_PASSWORD = 'lXl931207'
 # EMAIL_SUBJECT_PREFIX = u'Grumblr'
-DEFAULT_FROM_EMAIL = 'Grumblr.xianlal<larry.l1@hotmail.com>'
+DEFAULT_FROM_EMAIL = 'Grumblr.xianlal<larry.12.77.07@gmail.com>'
 EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = True
 
